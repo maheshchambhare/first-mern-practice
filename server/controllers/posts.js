2a1849fbb81e7c2d5ef5ac72c93787be6478a4bd
mahesh <chambharem7@gmail.com>
2021-02-19 13:46:41 +0530

    tried-out

---
+++ server/controllers/posts.js
@@ -0,0 +1,17 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPost = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = (req, res) => {
+  res.send("PostCreation");
+};
